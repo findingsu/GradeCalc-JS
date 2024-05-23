40e773b75a211162a0b7e6a0c0e7be55e1a26650
Sumeya <sumeyabashraheeel@gmail.com>
2024-05-24 00:33:16 +0100

    calc

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
 
 function calculateGrade()
 {
-    var marks = Number(document.getElementById("txtMarks").value);
-    if( (marks>=0) && (marks<59) )
-    {
-      document.getElementById("spnGrade").innerHTML = "F";
+    const marks = Number(document.querySelector("#txtMarks").value);
+    if( (marks>=0) && (marks<59) ) {
+      document.querySelector("#spnGrade").innerHTML = "F";
+      logGrade("F")
     }
-    else if((marks>60)&& (marks<69))
-    {
-      document.getElementById("spnGrade").innerHTML = "D";
+    else if((marks>60)&& (marks<69)) {
+      document.querySelector("#spnGrade").innerHTML = "D";
+      logGrade("D")
     }
-    else if((marks>69)&& (marks<79))
-    {
-      document.getElementById("spnGrade").innerHTML = "C";
+    else if((marks>69)&& (marks<79)) {
+      document.querySelector("#spnGrade").innerHTML = "C";
+      logGrade("C")
     }
-    else if((marks>79)&& (marks<89))
-    {
-      document.getElementById("spnGrade").innerHTML = "B";
+    else if((marks>79)&& (marks<89)) {
+      document.querySelector("#spnGrade").innerHTML = "B";
+      logGrade("B")
+
     }
-    else if((marks>89)&& (marks<101))
-    {
-      document.getElementById("spnGrade").innerHTML = "A";
+    else if((marks>89)&& (marks<101)) {
+      document.querySelector("#spnGrade").innerHTML = "A";
+      logGrade("A")
     }
-     else 
-    {
-      document.getElementById("spnGrade").innerHTML = "Invalid Marks";
+     else {
+      document.querySelector("#spnGrade").innerHTML = "Invalid Marks";
+      logGrade("Invalid Marks")
     }
 }
+function logGrade(grade) {
+  console.log(grade)
+  // alert("Wow... Smooth brainer")
+}
+

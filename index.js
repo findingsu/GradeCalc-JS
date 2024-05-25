2487d5db3e2a51df8d3112f778f1aff8f8ddf16e
Sumeya <sumeyabashraheeel@gmail.com>
2024-05-25 01:31:51 +0100

    cleanup

--- index.js
+++ index.js
@@ -9,22 +9,22 @@ function calculateGrade()
       message.innerHTML = "Smooth brainer";
       message.style.color = 'red';
     }
-    else if((marks>59)&& (marks<69)) {
+    else if((marks>59) && (marks<69)) {
       grade.innerHTML = "D";
       message.innerHTML = "Do better next time";
       message.style.color = 'red';
     }
-    else if((marks>69)&& (marks<79)) {
+    else if((marks>69) && (marks<79)) {
       grade.innerHTML = "C";
       message.innerHTML = "Mediocre";
       message.style.color = 'darkorange';
     }
-    else if((marks>79)&& (marks<89)) {
+    else if((marks>79) && (marks<89)) {
       grade.innerHTML = "B";
       message.innerHTML = "You might have some potential";
       message.style.color = 'green';
     }
-    else if((marks>89)&& (marks<101)) {
+    else if((marks>89) && (marks<101)) {
       grade.innerHTML = "A";
       message.innerHTML = "Well done! Enjoy the academic validation";
       message.style.color = 'green';
